83df5739ad3ee4c4e1cdeaa03b356d0fa58e4e2a
Patricia Eihara <pateihara@gmail.com>
2023-10-16 19:20:06 -0300

    atualizacao

--- src/components/molecules/AppHeader.jsx
+++ src/components/molecules/AppHeader.jsx
@@ -14,8 +14,8 @@ import {
 import CIcon from '@coreui/icons-react'
 import { cilBell, cilEnvelopeOpen, cilList, cilMenu } from '@coreui/icons'
 
-import { AppBreadcrumb } from './index'
-import { AppHeaderDropdown } from './header/index'
+import { AppBreadcrumb } from '../index'
+import { AppHeaderDropdown } from '../header/index'
 import { logo } from 'src/assets/brand/logo'
 
 const AppHeader = () => {
